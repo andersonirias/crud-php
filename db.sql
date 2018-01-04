e407af35c455a7add613811c7ab9886a8747627d
Anderson Irias <anderson.irias@gmail.com>
2018-01-04 10:11:21 -0200

    creation of database already included in the script

--- db.sql
+++ db.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE examples;
+
+USE examples;
+
 CREATE TABLE `people` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL
+  `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
